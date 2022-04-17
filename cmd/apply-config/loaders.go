ed15fce6fd3d20d4336a1cdfd835792d3e37a18b
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-04-17 16:52:29 +0300

    Add support for jsonc

--- cmd/apply-config/loaders.go
+++ cmd/apply-config/loaders.go
@@ -1,11 +1,26 @@
 package main
 
-import "github.com/ZentriaMC/apply-config/internal/loader"
+import (
+	"bytes"
+	"io"
+
+	"github.com/ZentriaMC/apply-config/internal/loader"
+	"github.com/tidwall/jsonc"
+)
 
 var loaders = map[string]loader.Loader{
 	"yaml":       loader.YAMLLoader,
 	"yml":        loader.YAMLLoader,
 	"properties": loader.JavaPropertiesLoader,
 	"json":       loader.JSONLoader,
+	"jsonc": func(r io.Reader) (fd loader.FileData, err error) {
+		var buf bytes.Buffer
+		if _, err = io.Copy(&buf, r); err != nil {
+			return
+		}
+
+		jsoncBuf := jsonc.ToJSONInPlace(buf.Bytes())
+		return loader.JSONLoader(bytes.NewReader(jsoncBuf))
+	},
 	//"xml": nil
 }
